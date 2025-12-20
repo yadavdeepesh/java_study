8f233e2818981d1e7b4b3a42b44a9a36e251c73d
Deepesh Yadav <yadavdeepesh13@gmail.com>
2025-12-20 23:27:47 +0530

    Study Abstract class

---
+++ abstraction_in_java/TVMain.java
@@ -0,0 +1,10 @@
+package abstraction_in_java;
+
+public class TVMain {
+    public static void main(String[] args){
+        // create the object of class 
+        TvRemote tr = new TvRemote();
+        tr.turnOn();
+        tr.turnOff();
+    }
+}
---
+++ abstraction_in_java/TvRemote.java
@@ -0,0 +1,23 @@
+package abstraction_in_java;
+
+// create the abstract class
+abstract class TV {
+    // create the constructor in class
+    TV(){
+        System.out.println("I am using TV");
+    }
+    // create the abstract method..
+    abstract void turnOn();
+    abstract void turnOff();
+}
+
+public class TvRemote extends TV{
+    @Override
+    void turnOn(){
+        System.out.println("On The TV..");
+    }
+    @Override
+    void turnOff(){
+        System.out.println("Off the TV");
+    } 
+}
