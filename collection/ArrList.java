9cae05b10a599525476ae6ee263797658f4cc326
Deepesh Yadav <yadavdeepesh13@gmail.com>
2026-04-21 22:46:24 +0530

    study collection framework

---
+++ collection/ArrList.java
@@ -0,0 +1,28 @@
+package collection;
+
+import java.util.*;
+
+public class ArrList {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+
+        // add element
+        list.add("java");
+        list.add("c++");
+        list.add("php");
+        list.add("python");
+
+        // remove one value
+        list.remove("java");
+
+        // Printing the elements of the ArrayList
+        System.out.println("Programming Languages:");
+
+        for (String lang : list) {
+            System.out.println(lang);
+        }
+
+    }
+
+}
